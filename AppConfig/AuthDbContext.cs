9999af0d6ae8397f43c584521ae588d311a7a9c4
Christian20003 <christian.goellner@bnv-bamberg.de>
2025-02-04 18:31:46 +0100

    build: Downgrade EF-Core packages to work with mongoDB

--- AppConfig/AuthDbContext.cs
+++ AppConfig/AuthDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
 
 namespace FinBookeAPI.AppConfig;
 
@@ -12,11 +13,18 @@ public class AuthDbContext(
     IOptions<AuthDatabaseSettings> _settings
 ) : IdentityDbContext<DbUser>(options)
 {
+    //public DbSet<DbUser> Test { get; init; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
         var database = _settings.Value;
         var mongoClient = new MongoClient(database.ConnectionString);
         optionsBuilder.UseMongoDB(mongoClient, database.DatabaseName);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        //builder.Entity<DbUser>().ToCollection("random");
     }
 }
