7988866d5965c80f4feeba96f27269c12e753021
Christian20003 <christian.goellner@bnv-bamberg.de>
2025-09-24 17:43:35 +0200

    refactor: Adjust implementation of new data annotations for color and guid to be more capable

--- Attributes/ColorAttribute.cs
+++ Attributes/ColorAttribute.cs
@@ -5,7 +5,26 @@ namespace FinBookeAPI.Attributes;
 
 /// <summary>
 /// This class represents a new data annotation for class properties, fields and parameters that
-/// should be a valid color ('#rrggbb'). It should prevent that an unsupported color encoding is provided.
+/// should be a valid color encoding. It should prevent that an unsupported color encoding is provided.
+/// The following encodings are supported:
+/// <list type="bullet">
+///     <item>
+///         <b>Hexadecimal color coding:</b> Each value (red, green and blue) is encoded with a hexadecimal value.
+///         For example - <c>#fa23a6</c>
+///     </item>
+///     <item>
+///         <b>RGB color coding:</b> Each value (red, green and blue) is encoded with an integer value (max. 255).
+///         For example - <c>rgb(43, 56, 255)</c>
+///     </item>
+///     <item>
+///         <b>CMYK color coding:</b> Each value (cyan, magenta, yellow, black) is encoded with a percentage value.
+///         For example - <c>cmyk(15, 55, 98, 30)</c>
+///     </item>
+///     <item>
+///         <b>HSL color coding:</b> Expresses a color with hue, saturation and lightness.
+///         For example - <c>hsl(250, 28%, 8%)</c>
+///     </item>
+/// </list>
 /// </summary>
 [AttributeUsage(
     AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
@@ -13,6 +32,14 @@ namespace FinBookeAPI.Attributes;
 )]
 public class ColorAttribute : ValidationAttribute
 {
+    private readonly string _hexColorCode = @"^#[0-9a-fA-F]{6}$";
+    private readonly string _rgbColorCode =
+        @"^rgb\((25[0-5]|2[0-4][0-9]|[1]?[0-9]{1,2}),(25[0-5]|2[0-4][0-9]|[1]?[0-9]{1,2}),(25[0-5]|2[0-4][0-9]|[1]?[0-9]{1,2})\)$";
+    private readonly string _cmykColorCode =
+        @"^cmyk\((100|\d{1,2}),(100|\d{1,2}),(100|\d{1,2}),(100|\d{1,2})\)$";
+    private readonly string _hslColorCode =
+        @"^hsl\((360|3[0-5][0-9]|[1-2]?[0-9]{1,2}),(100|\d{1,2})%,(100|\d{1,2})%\)$";
+
     /// <summary>
     /// This function proofs if the provided color is a supported encoding.
     /// </summary>
@@ -24,7 +51,10 @@ public class ColorAttribute : ValidationAttribute
         {
             return false;
         }
-        string pattern = @"^#[0-9a-fA-F]{6}$";
-        return Regex.IsMatch(value.ToString() ?? string.Empty, pattern);
+        string data = Regex.Replace(value.ToString() ?? string.Empty, @"\s+", string.Empty);
+        return Regex.IsMatch(data, _hexColorCode)
+            || Regex.IsMatch(data, _rgbColorCode)
+            || Regex.IsMatch(data, _cmykColorCode)
+            || Regex.IsMatch(data, _hslColorCode);
     }
 }
--- Attributes/GuidAttribute.cs
+++ Attributes/GuidAttribute.cs
@@ -4,8 +4,8 @@ namespace FinBookeAPI.Attributes;
 
 /// <summary>
 /// This class represents a new data annotation for class properties, fields and parameters that
-/// should be valid GUIDs. It should prevent that an invalid GUID is provided. It does not proof
-/// if the GUID is empty.
+/// should be valid GUIDs. It should prevent that an invalid GUID is provided. It is implemented
+/// for primitive types and all classes that implements the <c>IEnumerable</c> interface.
 /// </summary>
 [AttributeUsage(
     AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
@@ -14,16 +14,30 @@ namespace FinBookeAPI.Attributes;
 public class GuidAttribute : ValidationAttribute
 {
     /// <summary>
-    /// This function proofs if the provided GUID is a valid GUID object.
+    /// This function proofs if the provided object is a valid GUID or an
+    /// <c>IEnumerable</c> structure that contains valid GUIDs.
     /// </summary>
     /// <param name="value">The object that should be analyzed.</param>
-    /// <returns>True, if the object is a valid GUID, otherwise false</returns>
+    /// <returns>True, if the object is a valid GUID or contains valid GUIDs, otherwise false</returns>
     public override bool IsValid(object? value)
     {
         if (value is null)
         {
             return false;
         }
+        // Check if object is iterable (Check each item)
+        if (value is IEnumerable<object> list)
+        {
+            foreach (var item in list)
+            {
+                if (!IsValid(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        // Check primitive element
         return Guid.TryParse(value.ToString(), out _);
     }
 }
